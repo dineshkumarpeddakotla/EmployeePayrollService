4bd501252b36e7a36d87764d6d5637dcce6a3dbb
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-24 17:09:06 +0530

    ADDED: Print and Count Method for Number of Entries

---
+++ src/test/java/com/employeeplayrollservice/TestEmployeePayrollService.java
@@ -0,0 +1,26 @@
+package com.employeeplayrollservice;
+
+import com.employeepayrollservice.EmployeePayrollData;
+import com.employeepayrollservice.EmployeePayrollService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class TestEmployeePayrollService {
+    @Test
+    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+
+        EmployeePayrollData[] arrayOfEmps = {new EmployeePayrollData(1231,"Dinesh",45000.00),
+                                             new EmployeePayrollData(1232,"Kumar",40000.00),
+                                             new EmployeePayrollData(1233,"Bhushan",15000.00)
+                                            };
+
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        Assertions.assertEquals(3,entries);
+    }
+}
