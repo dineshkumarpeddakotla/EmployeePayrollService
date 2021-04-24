fde7c01c76bccb8960f46a6d477c65baf92aa448
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-24 16:54:26 +0530

    ADDED: Write data to the File

--- src/main/java/com/employeepayrollservice/EmployeePayrollData.java
+++ src/main/java/com/employeepayrollservice/EmployeePayrollData.java
@@ -1,11 +1,11 @@
 package com.employeepayrollservice;
 
-public class EmployeePayRollData {
+public class EmployeePayrollData {
     private int id;
     private String name;
     private Double salary;
 
-    public EmployeePayRollData(int id, String name, Double salary) {
+    public EmployeePayrollData(int id, String name, Double salary) {
         setId(id);
         setName(name);
         setSalary(salary);
@@ -33,5 +33,14 @@ public class EmployeePayRollData {
 
     public void setSalary(Double salary) {
         this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayRollData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
     }
 }
