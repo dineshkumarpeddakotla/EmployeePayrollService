ef816b630a6ed96b112330f72a22f3b751de9d63
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-24 12:37:41 +0530

    ADDED: File Operations like Check File Exists, Delete File and Check File Not Exist,Create Directory, Create Empty File, List Files, Directories as well as Files with Extension

---
+++ src/test/java/com/employeeplayrollservice/NIOFileApiTest.java
@@ -0,0 +1,53 @@
+package com.employeeplayrollservice;
+
+import com.employeepayrollservice.FileUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+public class NIOFileApiTest {
+    private static final String HOME = System.getProperty("Path","D:\\javafellowshipinternship\\EmployeePayRoll");
+    private static final String PLAY_WITH_NIO = "TempPlayGround";
+
+    @Test
+    public void givenPathWhenCheckedThenConfirm() throws IOException {
+        //Check File Existence
+        System.out.println("HOME: "+HOME);
+        Path homePath = Paths.get(HOME);
+        Assertions.assertTrue(Files.exists(homePath));
+
+        //Delete File and Check File Not Exists
+        Path playPath = Paths.get(HOME + "\\" + PLAY_WITH_NIO);
+        if (Files.exists(playPath)) FileUtils.deleteFiles(playPath.toFile());
+
+        Assertions.assertTrue(Files.notExists(playPath));
+
+        //Create Directory
+        Files.createDirectory(playPath);
+        Assertions.assertTrue(Files.exists(playPath));
+
+        //Create File
+        IntStream.range(1, 10).forEach(counter -> {
+            Path tempFile = Paths.get(playPath + "\\temp" + counter);
+            Assertions.assertTrue(Files.notExists(tempFile));
+            try {
+                Files.createFile(tempFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Assertions.assertTrue(Files.exists(tempFile));
+        });
+
+        //List Files, Directories along with Files with extensions
+        Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
+        Files.newDirectoryStream(playPath).forEach(System.out::println);
+        Files.newDirectoryStream(playPath, path -> path.toFile().isFile() &&
+                                                   path.toString().startsWith("temp"))
+             .forEach(System.out::println);
+    }
+}
